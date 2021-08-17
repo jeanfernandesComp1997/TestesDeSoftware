b2e3dbcaf98664bc764e2264b7d5f0ce92c28483
jeanfernandesComp1997 <jeanfernandes10@hotmail.com>
2021-08-16 21:12:05 -0300

    Adiconando Traits

---
+++ Features.Tests/01-Traits/ClienteTests.cs
@@ -0,0 +1,61 @@
+﻿using Features.Clientes;
+using System;
+using Xunit;
+
+namespace Features.Tests._01_Traits
+{
+    public class ClienteTests
+    {
+        [Fact(DisplayName = "Novo Clente Válido")]
+        [Trait("Categoria", "Cliente Trait Testes")]
+        public void Cliente_NovoCliente_DeveEstarValido()
+        {
+            // Arrange
+            var cliente = new Cliente(
+                Guid.NewGuid(),
+                nome: "John",
+                sobrenome: "Doe",
+                dataNascimento: DateTime.Now.AddYears(-24),
+                email: "johndoe@email.com",
+                ativo: true,
+                dataCadastro: DateTime.Now);
+
+            // Act
+            var result = cliente.EhValido();
+
+            // Assert
+            Assert.True(result);
+            Assert.Empty(cliente.ValidationResult.Errors);
+        }
+
+        [Fact(DisplayName = "Novo Clente Inválido")]
+        [Trait("Categoria", "Cliente Trait Testes")]
+        public void Cliente_NovoCliente_DeveEstarInvalido()
+        {
+            // Arrange
+            var cliente = new Cliente(
+                Guid.NewGuid(),
+                nome: "John",
+                sobrenome: "Doe",
+                dataNascimento: DateTime.Now,
+                email: "johndoe@email.com",
+                ativo: true,
+                dataCadastro: DateTime.Now);
+
+            // Act
+            var result = cliente.EhValido();
+
+            // Assert
+            Assert.False(result);
+            Assert.NotEmpty(cliente.ValidationResult.Errors);
+        }
+
+        // Arrange
+
+
+        // Act
+
+
+        // Assert
+    }
+}
